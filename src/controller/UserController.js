d639b7364079a7ffc570ed349337a7010931d4f6
Yasmin Hansen <yaa-h@hotmail.com>
2020-05-31 15:21:11 -0300

    Atualizando a controller do usuário

--- src/controller/UserController.js
+++ src/controller/UserController.js
@@ -11,6 +11,16 @@ module.exports = {
     async create(request, response){
         const { name, email, password, city, uf } = request.body;
     
+        const user = await connection('user')
+        .where('email', email)
+        .select('email')
+        .first();
+
+        if(user !== undefined)
+        {
+            return response.status(401).json({ error: 'E-mail já cadastrado.'});
+        }
+        
         await connection('user').insert({
             name,
             email,
