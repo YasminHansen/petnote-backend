5cb671df4cb4d7f88b91dbf1a1d07357ea96dd25
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-13 02:17:26 -0300

    Atualiza json

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ const routes = require('./routes'); //importa o arquivo com as rotas
 var app = express(); //armazena a aplicação
 
 var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
 app.listen(process.env.PORT || 3333); //manda a aplicação ouvir a porta 3333 -> localhost:3333
 //para rodar a aplicação basta rodar "npm start" no terminal na pasta backend
