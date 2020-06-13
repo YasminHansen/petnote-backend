b8ead9ea3e6cf31f240806331ad1d4bdbe178857
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-13 01:27:46 -0300

    atualizando session controller

--- src/controller/SessionController.js
+++ src/controller/SessionController.js
@@ -4,6 +4,12 @@ module.exports = {
     async create(request, response){
         const { email, password } = request.body;
 
+        if(email == null || password == null 
+            || email == undefined || password == undefined
+            || email == '' || password == ''){
+            return response.status(400).json({ error: 'Preencha os campos.'});
+        }
+
         const user = await connection('user')
         .where({
             'email': email,
@@ -14,7 +20,7 @@ module.exports = {
         .first();
 
         if(!user){
-            return response.status(400).json({ error: 'Usuário não encontrado.' });
+            return response.status(400).json({ error: 'Usuário não encontrado' });
         }
 
         return response.json(user);
