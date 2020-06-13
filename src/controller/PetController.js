11b650f20e5ed870b6197adeb4e7897f6b7b106d
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-13 01:26:37 -0300

    Atualizando o pet controller

--- src/controller/PetController.js
+++ src/controller/PetController.js
@@ -18,7 +18,7 @@ module.exports = {
     },
 
     async create(request, response){
-        const { name, age, weight, gender, castrated, disease, photo } = request.body;
+        const { name, age, weight, gender, castrated, specie, disease, photo } = request.body;
         const user_id = request.headers.authorization;
 
         const [id] = await connection('pets').insert({
@@ -27,6 +27,7 @@ module.exports = {
             weight,
             gender,
             castrated,
+            specie,
             disease,
             user_id,
             photo
@@ -36,7 +37,7 @@ module.exports = {
     },
 
     async edit(request, response){
-        const {id, name, age, weight, gender, castrated, disease, photo} = request.body;
+        const {id, name, age, weight, gender, castrated, specie, disease, photo} = request.body;
         const user_id = request.headers.authorization;
 
         // const pets = await connection('pets')
@@ -54,6 +55,7 @@ module.exports = {
             'weight': weight,
             'gender': gender,
             'castrated': castrated,
+            'specie': specie,
             'disease': disease,
             'photo': photo,
         });
