41a0540b200f29b91d10c462aceccc3474199408
Yasmin Hansen <yaa-h@hotmail.com>
2020-06-13 15:09:39 -0300

    Revert "Atualizando index.js"
    This reverts commit 2eaad16ec0771c6435639af109629677fa34623a.

--- src/index.js
+++ src/index.js
@@ -4,22 +4,19 @@ var cors = require('cors');
 const routes = require('./routes'); //importa o arquivo com as rotas
 var app = express(); //armazena a aplicação
 
-var bodyParser = require('body-parser');
-app.use(bodyParser.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-// app.post('/image-upload', (req, res) => {
+app.post('/image-upload', (req, res) => {
 
-//     const values = Object.values(req.files)
-//     const promises = values.map(image => cloudinary.uploader.upload(image.path))
+    const values = Object.values(req.files)
+    const promises = values.map(image => cloudinary.uploader.upload(image.path))
     
-//     Promise
-//       .all(promises)
-//       .then(results => res.json(results))
-//   })
+    Promise
+      .all(promises)
+      .then(results => res.json(results))
+  })
 
 app.listen(process.env.PORT || 3333); //manda a aplicação ouvir a porta 3333 -> localhost:3333
 //para rodar a aplicação basta rodar "npm start" no terminal na pasta backend
